0a2261d062127464410c8002663f913776340b6a
laboratorio <laboratorio@UPOAULA10707>
2025-07-08 07:22:50 -0500

    Agregar archivos de proyecto.

---
+++ MHerreraProgreso3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MHerreraProgreso3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MHerreraProgreso3/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MHerreraProgreso3;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
