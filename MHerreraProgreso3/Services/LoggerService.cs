7a1e0fe4f05f70614522de7b4854dfda2f60532b
MateoHerrera <mateo.herrera@udla.edu.ec>
2025-07-08 08:10:12 -0500

    Estructura para DataBase Local con SQlite

--- MHerreraProgreso3/Services/LoggerService.cs
+++ MHerreraProgreso3/Services/LoggerService.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SQLite;
 namespace MHerreraProgreso3.Services
 {
     class LoggerService
     {
+        private readonly string _logPath;
+        public LoggerService()
+        {
+            _logPath = Path.Combine(FileSystem.AppDataDirectory,"log.txt");
+        }
+        public async Task RegistrarAccionAsync(String mensaje)
+        {
+            string log = $"{DateTime.Now}:{mensaje}{Environment.NewLine}";
+            await File.AppendAllTextAsync(_logPath, log);
+        }
     }
 }
--- MHerreraProgreso3/Services/ProductoDatabase.cs
+++ MHerreraProgreso3/Services/ProductoDatabase.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using MHerreraProgreso3.Models;
 
 namespace MHerreraProgreso3.Services
 {
     class ProductoDatabase
     {
+        private readonly SQLiteAsyncConnection _database;
+        public ProductoDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Producto>().Wait();
+        }
+        public Task<List<Producto>> GetProductosAsync() => _database.Table<Producto>().ToListAsync();
+        public Task<int> SaveProductoAsync(Producto producto) => _database.InsertAsync(producto);
+        public Task<int> DeleteProductoAsync(Producto producto) => _database.DeleteAsync(producto);    
     }
 }
