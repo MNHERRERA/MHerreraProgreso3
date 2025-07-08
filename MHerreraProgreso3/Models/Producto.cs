c82532b2e15a67773c371dabf43492bc6f33efa3
MateoHerrera <mateo.herrera@udla.edu.ec>
2025-07-08 07:55:07 -0500

    Atributos para Producto Models

--- MHerreraProgreso3/Models/Producto.cs
+++ MHerreraProgreso3/Models/Producto.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SQLite;
 namespace MHerreraProgreso3.Models
 {
     class Producto
     {
+        [PrimaryKey, AutoIncrement]
+
+        public string Id { set; get; }
+        public string Nombre { set; get; }
+        public string Categoria { set; get; }
+        public int Cantidad { set; get; }
+        public decimal PrecioEstimado { set; get; }
     }
 }
